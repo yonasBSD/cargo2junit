318f134482f68969606f0e542a20299b8520f306
Yonas <yonas.y@gmail.com>
2024-11-23 15:39:56 -0500

    feat: Add install.sh for Ubuntu/Debian.

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+os=$(grep ^ID= /etc/os-release | cut -d'=' -f2-)
+arch=""
+case "${os}" in
+  ubuntu) arch=amd64.deb ;;
+  debian) arch=amd64.deb ;;
+  *) arch= ;;
+esac
+
+if test -z "$arch"; then
+  echo "Error: Unsupported OS" $(grep ^NAME= /etc/os-release | cut -d'=' -f2-)
+  return 1
+fi
+
+version=$(curl -sL https://api.github.com/repos/yonasBSD/cargo2junit/releases/latest | jq -r ".tag_name" | cut -d'v' -f2)
+bin=cargo2junit_${version}_${arch}
+wget https://github.com/yonasBSD/cargo2junit/releases/download/v${version}/${bin}
+sudo dpkg -i ${bin}
+rm ${bin}
